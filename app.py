ca73ddcf67018c1de1a8e9b8ae63a0bc2c6fc071
Unknown <12rixon@gmail.com>
2017-09-11 09:21:59 +0100

    Webserver and site created

---
+++ app.py
@@ -0,0 +1,29 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+
+@app.route('/')
+def hello():
+    """Renders a sample page."""
+    return "Hello World!"
+
+@app.route("/state")
+def state():
+    return render_template("html test.html", time = "10")
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(host='0.0.0.0')
