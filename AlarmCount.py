f9fca961b09ca45fb8fe8dc95674a9c69c4e9e50
Craig26 <31843513+Craig26@users.noreply.github.com>
2017-09-12 09:12:50 +0100

    Add files via upload
    Counts the amount of time the alarm has gone off - Dab

---
+++ AlarmCount.py
@@ -0,0 +1,7 @@
+set AlarmCount=0
+while BiscuitTin=False:
+    if WepbageTimer=0:
+        print("Enjoy your biscuit")
+    elif WebpageTimer>0:
+        print("GET AWAY FROM THE BISCUIT")
+        AlarmCount=(AlarmCount+1)
